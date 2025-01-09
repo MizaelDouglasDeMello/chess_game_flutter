eef3711e77528d52793f412647c9d0e4d011fe9c
MizaelDouglasDeMello <mizaeldouglas4@gmail.com>
2025-01-08 22:23:04 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/br/com/mizaeldouglas/chess_game_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.mizaeldouglas.chess_game_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/boardGame.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Boardgame extends StatefulWidget {
+  const Boardgame({super.key});
+
+  @override
+  State<Boardgame> createState() => _BoardgameState();
+}
+
+class _BoardgameState extends State<Boardgame> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
